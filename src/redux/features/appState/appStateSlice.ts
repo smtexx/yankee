045566744f5cc7713df1b25a1c88caaba9f3b078
Roman Kolmakov <smtexx@hotmail.com>
2022-12-12 10:12:25 +0100

    Create appStateSlice

---
+++ src/redux/features/appState/appStateSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Product, SoldProduct, User } from 'types';
+import { Currency, initialState, Lang } from './initialState';
+
+const appStateSlice = createSlice({
+  name: '@state',
+  initialState: initialState,
+  reducers: {
+    setLang(state, action: PayloadAction<Lang>) {
+      state.lang = action.payload;
+    },
+    setCurrency(state, action: PayloadAction<Currency>) {
+      state.currency = action.payload;
+    },
+    setUser(state, action: PayloadAction<User>) {
+      state.user = action.payload;
+    },
+    addToCart(state, action: PayloadAction<SoldProduct>) {
+      state.cart.push(action.payload);
+    },
+    addToFavorite(state, action: PayloadAction<Product['id']>) {
+      state.favorite.push(action.payload);
+    },
+  },
+});
+
+export default appStateSlice.reducer;
+export const {
+  addToCart,
+  addToFavorite,
+  setCurrency,
+  setLang,
+  setUser,
+} = appStateSlice.actions;
