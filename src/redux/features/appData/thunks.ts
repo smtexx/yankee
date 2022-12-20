28b12047756e284357dd77835f54b0c1b12bfe36
Roman Kolmakov <smtexx@hotmail.com>
2022-12-20 11:22:37 +0100

    Prevent resending the request if the status is LOADING

--- src/redux/features/appData/thunks.ts
+++ src/redux/features/appData/thunks.ts
@@ -1,18 +1,31 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BadResponseError, getUrl } from 'redux/helpers';
 import { mockFetch } from 'server/server';
-import { Category, Product, Path, Feature } from 'types';
+import { Category, Product, Path, Feature, AppData } from 'types';
 
 export const fetchProductsByCategory = createAsyncThunk<
   Product[],
-  Category | Feature
->('@data/fetchProductsByCategory', async (category) => {
-  const url = getUrl(Path.products, Path.category, category);
-  const response = await mockFetch(url);
+  Category | Feature,
+  { state: { data: AppData } }
+>(
+  '@data/fetchProductsByCategory',
+  async (category) => {
+    const url = getUrl(Path.products, Path.category, category);
+    const response = await mockFetch(url);
 
-  if (response.ok) {
-    return (await response.json()) as Product[];
+    if (response.ok) {
+      return (await response.json()) as Product[];
+    }
+
+    throw new BadResponseError(response.status);
+  },
+  {
+    condition: (_, api) => {
+      const state = api.getState();
+      const appStatus = state.data.status;
+      if (appStatus === 'LOADING') {
+        return false;
+      }
+    },
   }
-
-  throw new BadResponseError(response.status);
-});
+);
