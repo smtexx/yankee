39e934cdf084901815f0ba0ddde6896a61092ca2
Roman Kolmakov <smtexx@hotmail.com>
2022-12-20 12:39:46 +0100

    Create thunks for work with app state

---
+++ src/redux/features/appState/thunks.ts
@@ -0,0 +1,206 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BadResponseError, getUrl } from 'redux/helpers';
+import { mockFetch } from 'server/server';
+import {
+  ExchangeRate,
+  Method,
+  Path,
+  Product,
+  RegisteredOrder,
+  RegisteredUser,
+  UnregisteredUser,
+} from 'types';
+
+async function sendRequest<T, U>(
+  url: string,
+  login: string,
+  password: string,
+  method: Method,
+  body: T
+): Promise<U> {
+  const response = await mockFetch(url, {
+    method: method,
+    headers: {
+      Authorization: `Basic ${btoa(`${login}:${password}`)}`,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.ok) {
+    return (await response.json()) as U;
+  }
+
+  throw new BadResponseError(response.status);
+}
+
+export const registerUser = createAsyncThunk<
+  RegisteredUser,
+  {
+    login: string;
+    password: string;
+    user: UnregisteredUser;
+  }
+>('@state/registerUser', async ({ login, password, user }) => {
+  return await sendRequest(
+    getUrl(Path.user),
+    login,
+    password,
+    Method.PUT,
+    user
+  );
+});
+
+export const updateUserData = createAsyncThunk<
+  RegisteredUser,
+  {
+    login: string;
+    password: string;
+    user: UnregisteredUser;
+  }
+>('@state/updateUserData', async ({ login, password, user }) => {
+  return await sendRequest(
+    getUrl(Path.user),
+    login,
+    password,
+    Method.PATCH,
+    user
+  );
+});
+
+export const changePassword = createAsyncThunk<
+  {},
+  {
+    login: string;
+    password: string;
+    newPassword: string;
+  }
+>(
+  '@state/changePassword',
+  async ({ login, password, newPassword }) => {
+    return await sendRequest(
+      getUrl(Path.user, Path.password),
+      login,
+      password,
+      Method.PATCH,
+      { password: newPassword }
+    );
+  }
+);
+
+export const signIn = createAsyncThunk<
+  RegisteredUser,
+  {
+    login: string;
+    password: string;
+  }
+>('@state/signIn', async ({ login, password }) => {
+  return await sendRequest(
+    getUrl(Path.user, Path.sign),
+    login,
+    password,
+    Method.GET,
+    {}
+  );
+});
+
+export const createOrder = createAsyncThunk<
+  RegisteredOrder,
+  {
+    login: string;
+    password: string;
+    order: UnregisteredUser;
+  }
+>('@state/createOrder', async ({ login, password, order }) => {
+  return await sendRequest(
+    getUrl(Path.user, Path.orders),
+    login,
+    password,
+    Method.PUT,
+    order
+  );
+});
+
+export const addToFavorites = createAsyncThunk<
+  Product['id'][],
+  {
+    login: string;
+    password: string;
+    productID: string;
+  }
+>('@state/addToFavorites', async ({ login, password, productID }) => {
+  return await sendRequest(
+    getUrl(Path.user, Path.favorites, productID),
+    login,
+    password,
+    Method.GET,
+    {}
+  );
+});
+
+export const deleteFromFavorites = createAsyncThunk<
+  Product['id'][],
+  {
+    login: string;
+    password: string;
+    productID: string;
+  }
+>(
+  '@state/deleteFromFavorites',
+  async ({ login, password, productID }) => {
+    return await sendRequest(
+      getUrl(Path.user, Path.favorites, productID),
+      login,
+      password,
+      Method.DELETE,
+      {}
+    );
+  }
+);
+
+export const getUserFavorites = createAsyncThunk<
+  Product[],
+  {
+    login: string;
+    password: string;
+  }
+>('@state/getUserFavorites', async ({ login, password }) => {
+  return await sendRequest(
+    getUrl(Path.user, Path.favorites),
+    login,
+    password,
+    Method.GET,
+    {}
+  );
+});
+
+export const subscribe = createAsyncThunk<
+  {},
+  {
+    email: string;
+  }
+>('@state/subscribe', async ({ email }) => {
+  const response = await mockFetch(getUrl(Path.subscribe), {
+    method: Method.POST,
+    headers: {},
+    body: JSON.stringify({ email }),
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new BadResponseError(response.status);
+});
+
+export const getExchangeRates = createAsyncThunk(
+  '@state/subscribe',
+  async () => {
+    const response = await mockFetch(getUrl(Path.exchange));
+
+    if (response.ok) {
+      return (await response.json()) as ExchangeRate;
+    }
+
+    throw new BadResponseError(response.status);
+  }
+);
