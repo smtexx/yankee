17745d1b7abee23889468e44029edc721157a2bd
Roman Kolmakov <smtexx@hotmail.com>
2022-12-23 11:09:16 +0100

    Install generate-react-cli

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,7 @@
+import styles from './TemplateName.module.scss';
+
+type Props = {};
+
+export default function TemplateName({}: Props) {
+  return <div className={styles.templateName}>TemplateName Component</div>;
+}
