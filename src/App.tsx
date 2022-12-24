add980680ed0f8c55d5bcd61842a1d79a1499f3f
Roman Kolmakov <smtexx@hotmail.com>
2022-12-24 08:39:40 +0100

    Add Layout and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { routes } from 'data/routes';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import Layout from 'components/Layout/Layout';
+import PageContact from 'components/PageContact/PageContact';
+import PageHome from 'components/PageHome/PageHome';
+
+const router = createBrowserRouter([
+  {
+    path: routes.root.path,
+    element: <PageHome route={routes.root} />,
+  },
+  {
+    path: routes.contact.path,
+    element: <PageContact route={routes.contact} />,
+  },
+]);
 
 export default function App() {
-  return <Layout />;
+  return (
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  );
 }
