67bc9ef4ce5c84897ac4a1cf9aeaf34d036bde36
Roman Kolmakov <smtexx@hotmail.com>
2022-12-24 11:24:20 +0100

    Create components

--- src/components/PageContact/PageContact.tsx
+++ src/components/PageContact/PageContact.tsx
@@ -1,8 +1,14 @@
+import MetaData from 'components/MetaData/MetaData';
 import { Route } from 'types';
 import s from './PageContact.module.scss';
 
 type Props = { route: Route };
 
 export default function PageContact({ route }: Props) {
-  return <div className={s.pageContact}>PageContact Component</div>;
+  return (
+    <>
+      <MetaData route={route} />
+      <div className={s.pageContact}>PageContact Component</div>;
+    </>
+  );
 }
--- src/components/PageHome/PageHome.tsx
+++ src/components/PageHome/PageHome.tsx
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { selectLang } from 'redux/selectors';
+import MetaData from 'components/MetaData/MetaData';
+
 import { Route } from 'types';
 import s from './PageHome.module.scss';
 
 type Props = { route: Route };
 
 export default function PageHome({ route }: Props) {
-  const lang = useSelector(selectLang);
-  useEffect(() => {
-    document.title = route[lang].title;
-  }, []);
-  return <div className={s.pageHome}>PageHome Component</div>;
+  return (
+    <>
+      <MetaData route={route} />
+      <div className={s.pageHome}>PageHome Component</div>;
+    </>
+  );
 }
