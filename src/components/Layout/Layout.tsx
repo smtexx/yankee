8ca8035b684a6dcf3c7bcb9b0826cae83001e091
Roman Kolmakov <smtexx@hotmail.com>
2022-12-24 08:39:20 +0100

    Add children props

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,20 @@
-import s from './Layout.module.scss';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectMessage } from 'redux/selectors';
 import Message from 'components/Message/Message';
+import s from './Layout.module.scss';
 
-export default function Layout() {
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
   const message = useSelector(selectMessage);
 
   return (
     <div className={s.layout}>
       <div className={s.menu}></div>
-      <div className={s.content}>
-        {/* Routes from react-router */}
-      </div>
+      <div className={s.content}>{children}</div>
       <div className={s.footer}></div>
       {message && (
         <div className={s.message}>
