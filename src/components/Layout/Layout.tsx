78744e2b2bc954efcbc1050633a17d9c23c0bb3c
Roman Kolmakov <smtexx@hotmail.com>
2023-01-03 11:54:37 +0100

    Insert Menu componetn

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectMessage } from 'redux/selectors';
+import { selectLang, selectMessage } from 'redux/selectors';
 import Message from 'components/Message/Message';
 import s from './Layout.module.scss';
+import Menu from 'components/Menu/Menu';
 
 interface Props {
   children: React.ReactNode;
@@ -10,15 +11,18 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   const message = useSelector(selectMessage);
+  const lang = useSelector(selectLang);
 
   return (
     <div className={s.layout}>
-      <div className={s.menu}></div>
+      <div className={s.menu}>
+        <Menu lang={lang} />
+      </div>
       <div className={s.content}>{children}</div>
       <div className={s.footer}></div>
       {message && (
         <div className={s.message}>
-          <Message text={message} />
+          <Message text={message} lang={lang} />
         </div>
       )}
     </div>
